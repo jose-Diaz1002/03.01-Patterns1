c1447b62c9413664ea677307f709242ca98f592a
JoseDiaz <jldi1002@hotmail.com>
2025-05-20 22:54:04 +0200

    formatendo el codigo con Ctrl + Alt + L

--- src/nivel1/Main.java
+++ src/nivel1/Main.java
@@ -1,7 +1,6 @@
 package nivel1;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
--- src/nivel1/Undo.java
+++ src/nivel1/Undo.java
@@ -7,44 +7,42 @@ public class Undo {
     private static Undo undo;
     private List<String> history;
 
-    private Undo(){
+    private Undo() {
 
         history = new ArrayList<>();
     }
 
-    public static Undo getInstance(){
-        if(undo == null){
+    public static Undo getInstance() {
+        if (undo == null) {
             undo = new Undo();
         }
         return undo;
     }
 
-    public void addOrder(String order){
+    public void addOrder(String order) {
 
         history.add(order);
         System.out.println("Order saved.");
 
     }
 
-    public void undoOrder(){
-        if (history.isEmpty()){
+    public void undoOrder() {
+        if (history.isEmpty()) {
 
             System.out.println("No orders to delete.");
 
-        }
-        else{
-            history.remove(history.size()-1);
+        } else {
+            history.remove(history.size() - 1);
             System.out.println("Last order deleted");
         }
 
     }
 
-    public void listOrders(){
-        if (history.isEmpty()){
+    public void listOrders() {
+        if (history.isEmpty()) {
             System.out.println("No orders to show.");
-        }
-        else{
-            for(int i = 0; i < history.size(); i++){
+        } else {
+            for (int i = 0; i < history.size(); i++) {
                 System.out.println(history.get(i));
             }
         }
